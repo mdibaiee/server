6b057f35247f64c363b7bfcbe2e3291759ce5c55
Mahdi Dibaiee <mdibaiee@aol.com>
2019-06-23 19:39:59 +0430

    fix(database): use Future and databaseContext

--- app/controllers/DatabaseController.scala
+++ app/controllers/DatabaseController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import javax.inject._
 
+import scala.concurrent.{Await, Future}
 import akka.actor.ActorSystem
 import play.api.mvc._
 import play.api.db._
@@ -13,28 +14,30 @@ case class Package(id: Int, name: String)
 
 @Singleton
 class DatabaseController @Inject() (db: Database, cc: ControllerComponents, databaseExecutionContext: DatabaseExecutionContext) extends AbstractController(cc) {
-  def index = Action {
+  def index = Action.async {
     implicit val packageJson = Json.format[Package]
 
     val conn = db.getConnection()
-    try {
-      val stmt = conn.createStatement
-      val rs   = stmt.executeQuery("SELECT * from packages")
+    Future {
+      try {
+        val stmt = conn.createStatement
+        val rs   = stmt.executeQuery("SELECT * from packages")
 
-      var pkgs: Array[Package] = Array()
-      while (rs.next()) {
-        pkgs = pkgs :+ Package(id = rs.getInt("id"), name = rs.getString("name"))
-      }
+        var pkgs: Array[Package] = Array()
+        while (rs.next()) {
+          pkgs = pkgs :+ Package(id = rs.getInt("id"), name = rs.getString("name"))
+        }
 
-      Ok(Json.toJson(pkgs))
-    } catch {
-      case e: Exception => {
-        Logger.error("error", e)
-        NotFound
+        Ok(Json.toJson(pkgs))
+      } catch {
+        case e: Exception => {
+          Logger.error("error", e)
+          NotFound
+        }
+      } finally {
+        conn.close()
       }
-    } finally {
-      conn.close()
-    }
+    }(databaseExecutionContext)
   }
 }
   
